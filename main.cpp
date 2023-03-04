ce6b1b89c722d3e5eae802eb9be6cbe2626abd08
EthanRule <ethan2002@live.com>
2023-03-04 09:40:19 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+Authors: Ethan Rule,
+Last Edited:
+
+Objective: Making the game Pong purely from C++, using no graphics librarys.
+
+*/
+#include <iostream>
+#include <cstdio>
+
+int main()
+{
+    int num = 10;        //%d
+    long num1 = 11;      //%ld
+    char char1 = 'a';    //%c
+    float fl1 = 1.11;    //%f
+    double fl2 = 1.1111; //%lf
+    std::cin >> num;
+    std::cout << num;
+    return 0;
+}
